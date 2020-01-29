af8bfa61044524129e969f5b28a4039575c2d4d4
Jessica Filippelli <j_fil@cslab-green.concordia.ca>
2020-01-29 11:48:35 -0500

    canvas

--- js/script.js
+++ js/script.js
@@ -13,6 +13,10 @@ author, and this description to match your project!
 // preload()
 //
 // Description of preload
+// let windowWidth = windowWidth;
+// let windowHeight = windowHeight;
+
+let canvas;
 
 function preload() {
 
@@ -24,14 +28,26 @@ function preload() {
 // Description of setup
 
 function setup() {
+canvas = createCanvas(window.innerWidth, window.innerHeight);
 
 }
-
+window.onresize = function() {
+let w = window.innerWidth;
+  let h = window.innerHeight;
+  canvas.size(w,h);
+  width = w;
+  height = h;
+};
 
 // draw()
 //
 // Description of draw()
 
 function draw() {
+  background(0, 0, 0);
+
+//location: x and y,
+  rect(10,5,20,18);
+//rect(x  y  w  h);
 
 }
