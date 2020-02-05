b18562b05573fb57a8d9c7c3c4ffd411fb8c5410
Jessica Filippelli <j_fil@cslab-green.concordia.ca>
2020-02-05 11:12:51 -0500

    turn everthing into HTML ELMENTS AND NOT P5

--- js/Cell.js
+++ js/Cell.js
@@ -1,12 +1,40 @@
 class Cell{
-  constructor(x,y,width,height) {
+  constructor(x,y,width,height,id) {
     this.height = height;
     this.width = width;
     this.x = x;
     this.y =y;
+
+    this.boxElement = document.createElement("div");
+    this.boxElement.className = ("cell");
+    this.boxElement.classList.add("off");
+
+    this.boxElement.id = id;
+    this.boxElement.style.width = this.width +"px";
+    this.boxElement.style.height = this.height +"px";
+    this.boxElement.style.top =this.y+"px";
+    this.boxElement.style.left = this.x +"px";
+     document.getElementById("wrapper").appendChild(this.boxElement);
+
+    this.boxElement.addEventListener("mouseover", this.mouseOverFunction);
+    //  this.boxElement.addEventListener("mouseout", this.mouseOutFunction);
+
   }
+
+  mouseOverFunction(event){
+    console.log(event.target);
+    event.target.classList.remove("off");
+    event.target.classList.add("hover");
+  }
+
+  // mouseOutFunction(event){
+  //   console.log(event.target);
+  //   event.target.classList.remove("hover");
+  //   event.target.classList.add("off");
+  // }
+
   display(){
-    fill(255);
-      rect(this.x,this.y,this.width,this.height);
+    //fill(255);
+      //rect(this.x,this.y,this.width,this.height);
   }
 }
--- js/script.js
+++ js/script.js
@@ -7,76 +7,27 @@
 let canvas;
 let cell;
 let grid=[];
-const NUM_COLS = 20;
-const NUM_ROWS = 10;
+const NUM_COLS = 40;
+const NUM_ROWS = 19;
 const SPACER = 5;
-const CELL_SIZE =40;
-//let changeColor =
-function preload() {
-
-}
+const CELL_SIZE =45;
 
 
 
+window.onload = setup;
 
 function setup() {
-canvas = createCanvas(window.innerWidth, window.innerHeight);
+  let index =0;
 for(let x =0; x< NUM_COLS;x++){
   // do each column
     for(let y =0; y<NUM_ROWS;y++){
-      let cell = new Cell((x*CELL_SIZE)+(SPACER*x), (y*CELL_SIZE)+(SPACER*y),CELL_SIZE,CELL_SIZE);
-
+      let cell = new Cell((x*CELL_SIZE)+(SPACER*x), (y*CELL_SIZE)+(SPACER*y),CELL_SIZE,CELL_SIZE,index);
+      index++;
       grid.push(cell);
     }
 
-//trying to change color of squares with mouse over
-canvas.mouseOver(changeColor);
-
-}
-//cell = new Cell (10,5,40,40);
-
-}
-window.onresize = function() {
-let w = window.innerWidth;
-  let h = window.innerHeight;
-  canvas.size(w,h);
-  width = w;
-  height = h;
-};
-
-// draw()
-//
-// Description of draw()
-
-function draw() {
-  background(0, 0, 0);
-  for(let i =0; i<grid.length; i++){
-    let cell = grid[i];
-    cell.display();
-
-
-//change color of squars
-
-
-
-
-
-
-
-  }
-  //cell.display();
-
 
 
 }
 
-
- function changeColor(){
-   console.log("hi");
-   if (NUM_COLS && NUM_ROWS ===true) {
-       fill(255);
-     } else {
-       fill(50);
-     }
-
- }
+}
