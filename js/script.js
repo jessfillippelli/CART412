957fd7117b1234a09b790a9974b9c7ac03b7dce6
Jessica Filippelli <j_fil@cslab-green.concordia.ca>
2020-02-10 15:39:13 -0500

    some of the  are not working

--- js/script.js
+++ js/script.js
@@ -12,6 +12,8 @@ const NUM_ROWS = 19;
 const SPACER = 5;
 const CELL_SIZE =45;
 let counter_q =0;
+ let counter_e =0;
+ let counter_r =0;
 
 
 
@@ -41,6 +43,7 @@ for(let x =0; x< NUM_COLS;x++){
 }
 
 function changeThem(){
+  // key q
   console.log(event.keyCode);
   if(event.keyCode ===81 ){
     counter_q = counter_q+1;
@@ -53,24 +56,22 @@ function changeThem(){
     else if(counter_q%2 ===1 && i%2===0){
      grid[i].cellColor = "black";
    }
-    else if(counter_q%2 ===0 && i%2===1) {
-        grid[i].cellColor = "black";
-    }
-    else{
-        grid[i].cellColor = "green";
-    }
+    // else if(counter_q%2 ===0 && i%2===1) {
+    //     grid[i].cellColor = "black";
+    // }
+    // else{
+    //     grid[i].cellColor = "green";
+    // }
       grid[i].boxElement.style.background = grid[i].cellColor;
     }
 
-  }
+  } //end of 81
 
 //one black square and one green till the end
 
 
-
-    //fill(10,150,71);
-//  }
 if(event.keyCode ===87){
+  //key w
     for(let i =0; i<grid.length;i++){
       let colorChoice = i%colorArray.length;
       grid[i].cellColor = colorArray[colorChoice];
@@ -78,9 +79,48 @@ if(event.keyCode ===87){
 
     }
 //the array of colors fuction
-}
+}// end of 87
+
+if(event.keyCode ===69){
+   counter_e = counter_e+1;
+  for(let i =0; i<grid.length;i++){
+
+// if counter is odd make it black else make it green
+   if(counter_q%12 ===1 ){
+    grid[i].cellColor = "green";
+  }
+ //  else if(counter_q%12 ===1 && i%12===0){
+ //   grid[i].cellColor = "green";
+ // }
+
+    grid[i].boxElement.style.background = grid[i].cellColor;
+  }
+
+} //end of 69
+
+if(event.keyCode ===82){
+  counter_r = counter_r+1;
+  for(let i =0; i<grid.length;i++){
+
+
+   if(counter_q%1 ===12 && i%12===1){
+    grid[i].cellColor = "green";
+  }
+  else if(counter_q%2 ===1 && i%2===0){
+   grid[i].cellColor = "black";
+ }
+
+    grid[i].boxElement.style.background = grid[i].cellColor;
+  }
+
+} //end of 82
+
+
+
+
+
+
+
 
 } //end of changeThem
-
-
-}
+}//end of set up
