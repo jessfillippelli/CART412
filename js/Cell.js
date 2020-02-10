dd935bccab48bec180c7d8aa7807ef68facefa25
Jessica Filippelli <j_fil@cslab-green.concordia.ca>
2020-02-10 14:35:32 -0500

    first vol of two stages of pressure from a user

--- js/Cell.js
+++ js/Cell.js
@@ -1,9 +1,3 @@
-
-document.addEventListener('keydown', changeThem);
-
-
-let colorArray = ["#D1D869", "#8ACFB9", "#B2D456", "#6CC28A", "#98BF62", "#71B744",
-"#35B44C", "#15AB4B", "#728C6A", "#597D38", "#088A44", "#354A23"];
 class Cell{
   constructor(x,y,width,height,id) {
     this.height = height;
@@ -20,41 +14,35 @@ class Cell{
     this.boxElement.style.height = this.height +"px";
     this.boxElement.style.top =this.y+"px";
     this.boxElement.style.left = this.x +"px";
+    // color of cell;
+    this.cellColor = "#000000";
+    this.boxElement.style.background = this.cellColor;
      document.getElementById("wrapper").appendChild(this.boxElement);
 
-    this.boxElement.addEventListener("mouseover", this.mouseOverFunction);
-    //  this.boxElement.addEventListener("mouseout", this.mouseOutFunction);
+
+  //this.boxElement.addEventListener("mouseover", this.mouseOverFunction);
+//  this.boxElement.addEventListener("mouseout", this.mouseOutFunction);
 
   }
 
   mouseOverFunction(event){
-    console.log(event.target);
-    event.target.classList.remove("off");
-    event.target.classList.add("hover");
+    //console.log(event.target);
+    //event.target.classList.remove("off");
+  //  event.target.classList.add("hover");
+    this.cellColor = "green";
+    event.target.style.background = this.cellColor;
   }
 
-function changeThem(){
-  console.log(event.keyCode);
-  if(event.keyCode ===81 ){
-
-//one black square and one green till the end
 
 
-    //fill(10,150,71);
+
+  mouseOutFunction(event){
+  //  console.log(event.target);
+  //  event.target.classList.remove("hover");
+  //  event.target.classList.add("off");
+  this.cellColor = "black";
+  event.target.style.background = this.cellColor;
   }
-if(event.keyCode ===87){
-//the array of colors fuction
-}
-
-} //end of changeThem
-
-
-
-  // mouseOutFunction(event){
-  //   console.log(event.target);
-  //   event.target.classList.remove("hover");
-  //   event.target.classList.add("off");
-  // }
 
   display(){
     //fill(255);
--- js/script.js
+++ js/script.js
@@ -11,12 +11,22 @@ const NUM_COLS = 40;
 const NUM_ROWS = 19;
 const SPACER = 5;
 const CELL_SIZE =45;
+let counter_q =0;
 
 
 
 window.onload = setup;
 
 function setup() {
+
+  document.addEventListener('keydown', changeThem);
+
+
+
+  let colorArray = ["#D1D869", "#8ACFB9", "#B2D456", "#6CC28A", "#98BF62", "#71B744",
+  "#35B44C", "#15AB4B", "#728C6A", "#597D38", "#088A44", "#354A23"];
+
+
   let index =0;
 for(let x =0; x< NUM_COLS;x++){
   // do each column
@@ -29,5 +39,48 @@ for(let x =0; x< NUM_COLS;x++){
 
 
 }
+
+function changeThem(){
+  console.log(event.keyCode);
+  if(event.keyCode ===81 ){
+    counter_q = counter_q+1;
+    for(let i =0; i<grid.length;i++){
+
+// if counter is odd make it black else make it green
+     if(counter_q%2 ===1 && i%2===1){
+      grid[i].cellColor = "green";
+    }
+    else if(counter_q%2 ===1 && i%2===0){
+     grid[i].cellColor = "black";
+   }
+    else if(counter_q%2 ===0 && i%2===1) {
+        grid[i].cellColor = "black";
+    }
+    else{
+        grid[i].cellColor = "green";
+    }
+      grid[i].boxElement.style.background = grid[i].cellColor;
+    }
+
+  }
+
+//one black square and one green till the end
+
+
+
+    //fill(10,150,71);
+//  }
+if(event.keyCode ===87){
+    for(let i =0; i<grid.length;i++){
+      let colorChoice = i%colorArray.length;
+      grid[i].cellColor = colorArray[colorChoice];
+      grid[i].boxElement.style.background = grid[i].cellColor;
+
+    }
+//the array of colors fuction
+}
+
+} //end of changeThem
+
 
 }
